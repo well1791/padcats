0bbfb3a148641e0ed33fd956741a2d0f4ff3a6fe
Wellington Mendoza <wellsaint91@gmail.com>
2023-10-22 16:04:41 -0500

    remove react-table for now

--- src/ui/template/podcasts/index.tsx
+++ src/ui/template/podcasts/index.tsx
@@ -1,13 +1,4 @@
 import { useState } from 'react'
-import {
-  // ColumnDef,
-  flexRender,
-  getCoreRowModel,
-  // getSortedRowModel,
-  // SortingState,
-  useReactTable,
-  createColumnHelper,
-} from '@tanstack/react-table'
 
 import * as st from './styles.css'
 import { cs } from '~/utils'
@@ -40,75 +31,11 @@ export type Props = {
   onPlayPause: (id: PodcastId) => void
 }
 
-const helper = createColumnHelper<Podcast>()
-
 function Podcasts({ data: d, ...p }: Props) {
-  const [isPlaying, setIsPlaying] = useState(false)
-  const columns = [
-    helper.accessor('trackNumber', {
-      header: '',
-      cell: () => (
-        <PlayPauseBtn
-          isPlaying={isPlaying}
-          isHighlighted={isPlaying}
-          onPlayPause={() => setIsPlaying(!isPlaying)}
-        />
-      ),
-    }),
-    helper.accessor('author', {
-      header: 'Name',
-      cell: (info) => (
-        <PlayerInfo
-          className={st.playerInfo}
-          data={{
-            title: 'asdsd',
-            author: 'lkjl',
-            img: {
-              src: 'dsj',
-              alt: ','
-            }
-          }}
-        />
-      ),
-    }),
-    helper.accessor('description', {
-      header: 'Description',
-    }),
-    helper.accessor('releasedAt', {
-      header: 'Released',
-    }),
-  ]
-
-  const table = useReactTable({
-    data: Object.values(d.podcasts),
-    columns,
-    getCoreRowModel: getCoreRowModel(),
-  })
 
   return (
-    <table className={cs(st.container, p.className)}>
-      <thead>
-        <tr>
-          {table.getFlatHeaders().map((h) => (
-            <th key={h.id}>
-              {flexRender(h.column.columnDef.header, h.getContext())}
-            </th>
-          ))}
-        </tr>
-      </thead>
-
-      <tbody>
-        {table.getRowModel().rows.map((r) => (
-          <tr key={r.id}>
-            {r.getVisibleCells().map((c) => (
-              <td key={c.id}>
-                {flexRender(c.column.columnDef.cell, c.getContext())}
-              </td>
-            ))}
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <div className={cs(st.container, p.className)}>
+    </div>
   )
 }
 
